0f3020cbc74c0fb2e6e3eb1c487c8ea8e483d182
ejeong4 <106835807+ejeong4@users.noreply.github.com>
2025-03-12 18:30:19 -0400

    fix script error

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function () {
+    console.log("Checking if codapInterface is defined...");
     if (typeof codapInterface !== "undefined" && codapInterface.init) {
         codapInterface.init();
+        console.log("interface loaded.")
     } else {
         console.error("codapInterface is not defined! Make sure codap-helper.js is loaded first.");
     }
